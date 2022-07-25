8e4e2731fc2d09ca0eda0fc260941a3098fcf4ba
zhaow-01 <852322127@qq.com>
2022-07-25 23:47:52 +0800

    test

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     author_email='852322127@qq.com',
     packages=find_packages(),
     zip_safe=False,
-    python_requires='>=3.5',
+    python_requires='>=3.5  ',
     setup_requires = ["pybind11 >= 2.9.2"],
     install_requires=["numpy >= 1.19.0",
                       "pandas >=1.1.0",
@@ -40,6 +40,7 @@ setup(
                       "polars >= 0.13.51",
                       "pybind11>= 2.9.2",
                       "matplotlib",
+                      "llvmlite = 0.38.0"
                       "numba"
                       ],
     include_package_data=True,
